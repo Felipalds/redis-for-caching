7d6cbce40b9f7452b72bfb554da552a024b8e124
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2023-03-15 17:02:21 -0300

    caching simple

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,38 @@
 import express, { Express, Request, Response } from "express"
 import dotenv from "dotenv"
-
+import redis, { createClient } from "redis"
 dotenv.config()
 
 const app:Express = express()
+const client = createClient()
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("<h1> Hello world </h1>")
+const getAllProducts = () => {
+    const time = Math.random() * 5000
+    return new Promise((resolve) => {
+        setTimeout(async() => {
+            await client.set("allProducts", JSON.stringify([ "Produto 1", "Produto 2", "Produto 3"]), { EX : 10})
+            resolve([ "Produto 1", "Produto 2", "Produto 3"])
+        }, 5000)
+    })
+}
+
+app.get("/", async (req: Request, res: Response) => {
+    await client.set("MAE", "ABC")
+    const value = await client.get("allProducts")
+    console.log(value);
+    if(value){
+        return res.send(JSON.parse(value))
+    } else {
+        return res.send(await getAllProducts())
+    }
 })
 
-app.listen(3000, () => {
-    console.log("APP STARTED AT 3000")
-})
+const startup = async () => {
+    await client.connect()
+    app.listen(3000, () => {
+        console.log("APP STARTED AT 3000")
+    })
+}
+
+startup()
+
